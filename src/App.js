b01d10e4ee7afb2866a6b1617c3670316f5b05a7
Tristan Barrow <tristan.barrow@workmax.com>
2020-11-23 14:34:30 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const [showingRef, setShowingRefTo] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='page'>
+
+      <div className='msg'>Clicking outside of the ref will remove the ref.</div>
+      {showingRef && (
+        <RefComp 
+          close={() => setShowingRefTo(false)}
+        />
+      )}
+
+      <button onClick={() => window.location.reload()}>Reset</button>
     </div>
   );
 }
+
+const RefComp = (props) => {
+  const [showingDiv, setShowingDivTo] = useState(true);
+  const ref = useRef(null);
+  
+  // close ref on clicking outside the ref
+  useEffect(() => {
+    const handleClick = (e) => {
+      if (ref.current.contains(e.target)) return;
+      props.close();
+    }
+    document.addEventListener('click', handleClick);
+    return () => {
+      document.removeEventListener('click', handleClick);
+      props.close(false);
+    } 
+
+  },[]);
+  
+  return (
+    <div className='ref' ref={ref}>
+      <div
+        className='btn1'
+        onClick={() => setShowingDivTo(!showingDiv)} 
+      >
+        toggle the div below (I don't have problems)
+      </div>
+
+      {showingDiv && (
+        <div 
+          className='btn2'
+          onClick={() => setShowingDivTo(false)} 
+        >
+          Remove myself (I have problems)
+        </div>
+      )}
+    </div>
+  );
+}
+
 
 export default App;
